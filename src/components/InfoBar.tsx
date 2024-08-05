fa3adda29426d3149b5b2efaa455051d8056d058
NeoNavagis <herminio.gregorio@navagis.com>
2024-08-05 23:02:48 +0800

    fix: adjust infobar padding

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -85,25 +85,19 @@ export default function InfoBar({ store, open, toggleDrawer }: InfoBarProps) {
         },
       }}
     >
-      <div className="m-5 p-5">
+      <div className="m-5 px-5">
         <List>
           <ListItem className="text-2xl font-bold">
             {store?.name ?? "Select a store to view more information"}
           </ListItem>
 
           {display.map((info) => (
-            <>
-              <Tooltip
-                key={info.text}
-                title={info.text}
-                placement="bottom-start"
-              >
-                <ListItem className="hoverable">
-                  <ListItemIcon>{info.icon}</ListItemIcon>
-                  {info.data ?? "N/A"}
-                </ListItem>
-              </Tooltip>
-            </>
+            <Tooltip key={info.text} title={info.text} placement="bottom-start">
+              <ListItem className="hoverable">
+                <ListItemIcon>{info.icon}</ListItemIcon>
+                {info.data ?? "N/A"}
+              </ListItem>
+            </Tooltip>
           ))}
           <ListItem className="flex justify-center items-center mt-2">
             <img src={store.store_id + ".jpg"} />
